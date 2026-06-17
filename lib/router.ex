05a349161b5b22691b38c1e7048ce01710cedafa
Vova Potiichuk <volodymyr.potiichuk@prosapient.com>
2026-06-17 19:31:19 +0300

    feat: router

---
+++ lib/router.ex
@@ -0,0 +1,54 @@
+defmodule DS.Router do
+  @slots 1024
+  @bucket_size 10
+
+  def which_node(key) do
+    slot = slot(key)
+    DS.Storage.Routing.get_node(slot)
+  end
+
+  def which_nodes_for_range(field, min, max) do
+    first_bucket = resolve_first_bucket(field, min)
+    last_bucket = resolve_last_bucket(field, max)
+
+    first_bucket
+    |> Stream.iterate(&(&1 + @bucket_size))
+    |> Stream.take_while(&(&1 <= last_bucket))
+    |> Enum.map(fn bucket_start ->
+      slot = :erlang.phash2({field, bucket_start}, @slots)
+      DS.Storage.Routing.get_node(slot)
+    end)
+    |> Enum.filter(&match?({:ok, _}, &1))
+    |> Enum.map(fn {:ok, node} -> node end)
+    |> Enum.uniq()
+  end
+
+  # Private
+  defp slot(key), do: :erlang.phash2(key, @slots)
+
+  defp resolve_first_bucket(_field, min) when is_number(min) do
+    div(min, @bucket_size) * @bucket_size
+  end
+
+  defp resolve_first_bucket(field, :negative_infinity) do
+    index_name = :"index_#{field}"
+
+    case :ets.first(index_name) do
+      :"$end_of_table" -> 0
+      {value, _} -> div(value, @bucket_size) * @bucket_size
+    end
+  end
+
+  defp resolve_last_bucket(_field, max) when is_number(max) do
+    div(max, @bucket_size) * @bucket_size
+  end
+
+  defp resolve_last_bucket(field, :infinity) do
+    index_name = :"index_#{field}"
+
+    case :ets.last(index_name) do
+      :"$end_of_table" -> 0
+      {value, _} -> div(value, @bucket_size) * @bucket_size
+    end
+  end
+end
